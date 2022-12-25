60fe1323d989a367d8bae0cc603b3550e2270c7a
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2022-12-25 09:47:27 +0530

    two

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Routes,Route,useNavigate} from 'react-router-dom';
+import Login from "./components/Login";
+import Home from "./container/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <Routes>
+        <Route path="Login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
           Learn React
-        </a>
-      </header>
+        
     </div>
   );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { useNavigate } from 'react-router-dom';
+import {FcGoogle} from 'react-icons/fc';
+import logo from "../assets/logo.png";
+import share from "../assets/share.mp4";
+
+const Login = () => {
+  return (
+    <div className='flex justify-start items-center flex-col h-screen'>login</div>
+  )
+}
+
+export default Login
